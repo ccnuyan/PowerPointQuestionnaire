d40839eb103f695fb9b671e9891ee6bf3a8a8c78
ccnuyan <ccnuyan@live.com>
2015-09-25 08:58:19 +0800

    just build

---
+++ PowerPointQuestionnaire/QuestionnaireRibbon.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace PowerPointQuestionnaire
+{
+    partial class QuestionnaireRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public QuestionnaireRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab = this.Factory.CreateRibbonTab();
+            this.authGroup = this.Factory.CreateRibbonGroup();
+            this.usernameLabel = this.Factory.CreateRibbonLabel();
+            this.loginButton = this.Factory.CreateRibbonButton();
+            this.slideOperationGroup = this.Factory.CreateRibbonGroup();
+            this.addNewQuestionnaireSlideButton = this.Factory.CreateRibbonButton();
+            this.setSlideButton = this.Factory.CreateRibbonButton();
+            this.buttonCancel = this.Factory.CreateRibbonButton();
+            this.errorLabel = this.Factory.CreateRibbonLabel();
+            this.tab.SuspendLayout();
+            this.authGroup.SuspendLayout();
+            this.slideOperationGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab
+            // 
+            this.tab.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab.Groups.Add(this.authGroup);
+            this.tab.Groups.Add(this.slideOperationGroup);
+            this.tab.Label = "PPT问卷";
+            this.tab.Name = "tab";
+            // 
+            // authGroup
+            // 
+            this.authGroup.Items.Add(this.usernameLabel);
+            this.authGroup.Items.Add(this.loginButton);
+            this.authGroup.Label = "认证/登陆";
+            this.authGroup.Name = "authGroup";
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.Label = "您还没有登录";
+            this.usernameLabel.Name = "usernameLabel";
+            // 
+            // loginButton
+            // 
+            this.loginButton.Label = "登陆";
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.loginButton_Click);
+            // 
+            // slideOperationGroup
+            // 
+            this.slideOperationGroup.Items.Add(this.addNewQuestionnaireSlideButton);
+            this.slideOperationGroup.Items.Add(this.setSlideButton);
+            this.slideOperationGroup.Items.Add(this.buttonCancel);
+            this.slideOperationGroup.Items.Add(this.errorLabel);
+            this.slideOperationGroup.Label = "设置";
+            this.slideOperationGroup.Name = "slideOperationGroup";
+            this.slideOperationGroup.Visible = false;
+            // 
+            // addNewQuestionnaireSlideButton
+            // 
+            this.addNewQuestionnaireSlideButton.Label = "添加新问卷页";
+            this.addNewQuestionnaireSlideButton.Name = "addNewQuestionnaireSlideButton";
+            this.addNewQuestionnaireSlideButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.addNewQuestionnaireSlideButton_Click);
+            // 
+            // setSlideButton
+            // 
+            this.setSlideButton.Label = "设置当前页为问卷";
+            this.setSlideButton.Name = "setSlideButton";
+            this.setSlideButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.setSlideButton_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Label = "取消问卷标记";
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.buttonCancel_Click);
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.Label = " ";
+            this.errorLabel.Name = "errorLabel";
+            // 
+            // QuestionnaireRibbon
+            // 
+            this.Name = "QuestionnaireRibbon";
+            this.RibbonType = "Microsoft.PowerPoint.Presentation";
+            this.Tabs.Add(this.tab);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.QuestionnaireRibbon_Load);
+            this.tab.ResumeLayout(false);
+            this.tab.PerformLayout();
+            this.authGroup.ResumeLayout(false);
+            this.authGroup.PerformLayout();
+            this.slideOperationGroup.ResumeLayout(false);
+            this.slideOperationGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup authGroup;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton loginButton;
+        private Microsoft.Office.Tools.Ribbon.RibbonTab tab;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup slideOperationGroup;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton setSlideButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton addNewQuestionnaireSlideButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton buttonCancel;
+        internal Microsoft.Office.Tools.Ribbon.RibbonLabel usernameLabel;
+        internal Microsoft.Office.Tools.Ribbon.RibbonLabel errorLabel;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal QuestionnaireRibbon QuestionnaireRibbon
+        {
+            get { return this.GetRibbon<QuestionnaireRibbon>(); }
+        }
+    }
+}
---
+++ PowerPointQuestionnaire/QuestionnaireRibbon.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms.Layout;
+using System.Windows.Forms.VisualStyles;
+using Microsoft.Office.Tools.Ribbon;
+using Newtonsoft.Json;
+using PowerPointQuestionnaire.Components;
+using PowerPointQuestionnaire.Controls;
+using PowerPointQuestionnaire.Interfaces;
+using PowerPointQuestionnaire.Model;
+using PowerPointQuestionnaire.Services;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Office = Microsoft.Office.Core;
+
+namespace PowerPointQuestionnaire
+{
+    public partial class QuestionnaireRibbon
+    {
+        private LoginWindow _loginWindow;
+        private QuestionnaireOptionsWindow _questionnaireOptionsWindow;
+
+        private const string ChoiceString = "ABCDEFGH";
+
+        private PowerPoint.Slide _selectedSlide;
+
+        private readonly IQuestionnaireUtil _questionnaireUtil = new QuestionnaireUtil();
+
+        private void loginButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            TryLogin();
+        }
+
+        private void QuestionnaireRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+            RefreshSetAndAdd();
+
+            AppWapper.App.SlideSelectionChanged += App_SlideSelectionChanged;
+            AppWapper.App.PresentationOpen += App_PresentationOpen;
+            AppWapper.App.PresentationSave += App_PresentationSave;
+
+            _loginWindow = new LoginWindow();
+            _loginWindow.Closed += LoginWindowClosed;
+
+            _questionnaireOptionsWindow = new QuestionnaireOptionsWindow();
+            _questionnaireOptionsWindow.Closed += QuestionnaireOptionsWindowClosed;
+        }
+
+        private void LoginWindowClosed(object sender, EventArgs e)
+        {
+            _loginWindow = new LoginWindow();
+            _loginWindow.Closed += LoginWindowClosed;
+        }
+
+        private void QuestionnaireOptionsWindowClosed(object sender, EventArgs e)
+        {
+            _questionnaireOptionsWindow = new QuestionnaireOptionsWindow();
+            _questionnaireOptionsWindow.Closed += QuestionnaireOptionsWindowClosed;
+        }
+
+        private void App_PresentationOpen(PowerPoint.Presentation Pres)
+        {
+            RefreshSetAndAdd();
+        }
+
+        private void TryLogin()
+        {
+            if (_loginWindow.ShowDialog() == true)
+            {
+                slideOperationGroup.Visible = true;
+                loginButton.Visible = false;
+                usernameLabel.Label = @"欢迎您，
+" + AuthService.Me.displayName;
+            }
+            else
+            {
+                slideOperationGroup.Visible = false;
+                loginButton.Visible = true;
+                usernameLabel.Label = "您尚未登陆";
+            }
+
+            RefreshSetAndAdd();
+        }
+
+
+
+
+        private void App_PresentationSave(PowerPoint.Presentation Pres)
+        {
+            foreach (PowerPoint.Slide slide in Pres.Slides)
+            {
+                if (!_questionnaireUtil.Check(slide)) continue;
+
+                var questionnaire = _questionnaireUtil.Deserialize(slide);
+
+                if (questionnaire.user != AuthService.Me._id.ToString())continue;
+
+                Task.Factory.StartNew(() =>
+                {
+                    UpdateSelectedSlide(slide, questionnaire);
+                });
+            }
+        }
+
+
+        public void RefreshSetAndAdd()
+        {
+            var slide = _selectedSlide;
+
+            if (AuthService.Me == null || slide == null)
+            {
+                setSlideButton.Visible = false;
+                buttonCancel.Visible = false;
+                return;
+            }
+
+            if (_questionnaireUtil.Check(slide))
+            {
+                var questionnaire = _questionnaireUtil.Deserialize(slide);
+
+                if (questionnaire.user != AuthService.Me._id.ToString())
+                {
+                    setSlideButton.Visible = false;
+                    buttonCancel.Visible = false;
+
+                    errorLabel.Label = "该页的问卷不属于你";
+                }
+                else
+                {
+                    setSlideButton.Visible = true;
+                    buttonCancel.Visible = true;
+
+                    setSlideButton.Label = "重新设置问卷";
+
+                    errorLabel.Label = " ";
+
+                    Task.Factory.StartNew(() =>
+                    {
+                        UpdateSelectedSlide(slide, questionnaire);
+                    });
+                }
+            }
+            else
+            {
+                setSlideButton.Visible = true;
+                buttonCancel.Visible = false;
+
+                setSlideButton.Label = "将当前选中页设置为问卷";
+            }
+        }
+
+        private PowerPoint.Slide AddSlideByQuestionnaire(QuestionnaireModel questionnaire)
+        {
+            var index = (_selectedSlide?.SlideIndex ?? Globals.ThisAddIn.Application.ActivePresentation.Slides.Count) + 1;
+
+            var slide = AppWapper.App.ActivePresentation.Slides.Add(index, PowerPoint.PpSlideLayout.ppLayoutBlank);
+
+            var textbox = slide.Shapes.AddTextbox(Office.MsoTextOrientation.msoTextOrientationHorizontal, 50, 50, 600, 50);//向当前PPT添加文本框
+            textbox.TextFrame.TextRange.Text = questionnaire.allowMultiple ? "调查(多选)" : "调查:";
+            textbox.TextFrame.TextRange.Font.Size = 48;//设置文本字体大小
+
+            for (var i = 0; i < questionnaire.choices; i++)
+            {
+                var choiceTextbox = slide.Shapes.AddTextbox(Office.MsoTextOrientation.msoTextOrientationHorizontal, 50, 120 + 40 * i, 400, 50);//向当前PPT添加文本框
+                choiceTextbox.TextFrame.TextRange.Text = ChoiceString[i] + ":";
+                choiceTextbox.TextFrame.TextRange.Font.Size = 24;//设置文本字体大小
+            }
+
+            _selectedSlide = slide;
+
+            return _selectedSlide;
+        }
+
+        private async void CreateQuestionnaireSlideRecord(PowerPoint.Slide slide, QuestionnaireModel questionnaire)
+        {
+            var tempFile = await _questionnaireUtil.UploadAsync(slide);
+            questionnaire.tempFileId = tempFile._id;
+
+            try
+            {
+                var created = await _questionnaireUtil.CreateAsync(questionnaire);
+                _questionnaireUtil.Mark(slide, created);
+            }
+            catch (WebException)
+            {
+                slide.Delete();
+                MessageBox.Show("问卷添加失败了,这可能是个网络错误");
+            }
+        }
+
+        private async void UpdateSelectedSlide(PowerPoint.Slide slide, QuestionnaireModel questionnaire)
+        {
+            var questionnaireToUpdate = _questionnaireUtil.Get(questionnaire.id);
+
+            var md5Now = _questionnaireUtil.GetSlideMd5(slide);
+
+            var flag = false;
+
+            if (questionnaireToUpdate.image == null || questionnaireToUpdate.image.md5 != md5Now)
+            {
+                flag = true;
+                var tempFile = await _questionnaireUtil.UploadAsync(slide);
+                questionnaireToUpdate.tempFileId = tempFile._id;
+            }
+
+            if (!_questionnaireUtil.Compare(questionnaireToUpdate, questionnaire))
+            {
+                flag = true;
+                questionnaireToUpdate.choices = questionnaire.choices;
+                questionnaireToUpdate.allowMultiple = questionnaire.allowMultiple;
+            }
+
+            if (!flag) return;
+
+            try
+            {
+                var updated = await _questionnaireUtil.UpdateAsync(questionnaireToUpdate);
+                _questionnaireUtil.Mark(slide, updated);
+            }
+            catch
+            {
+                MessageBox.Show("问卷同步失败了,这可能是个网络错误");
+            }
+        }
+
+
+
+        private void App_SlideSelectionChanged(PowerPoint.SlideRange SldRange)
+        {
+            if (SldRange.Count != 1)
+            {
+                _selectedSlide = null;
+            }
+            else
+            {
+                _selectedSlide = SldRange[1];
+            }
+            RefreshSetAndAdd();
+        }
+
+        private void addNewQuestionnaireSlideButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            if (_questionnaireOptionsWindow.ShowDialog() == true)
+            {
+                var questionnaire = new QuestionnaireModel()
+                {
+                    allowMultiple = _questionnaireOptionsWindow.AllowMultipleCheck.IsChecked == true,
+                    choices = (int)_questionnaireOptionsWindow.ChoicesComboBox.SelectionBoxItem
+                };
+
+                var slide = AddSlideByQuestionnaire(questionnaire);
+
+                CreateQuestionnaireSlideRecord(slide, questionnaire);
+            }
+        }
+
+        private void setSlideButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            var slide = _selectedSlide;
+
+            var window = new QuestionnaireOptionsWindow();
+            if (_questionnaireUtil.Check(slide))
+            {
+                window.Initialize(_questionnaireUtil.Deserialize(slide));
+            }
+
+            if (window.ShowDialog() == true)
+            {
+                try
+                {
+                    if (!_questionnaireUtil.Check(slide))
+                    {
+                        //CreateQuestionnaireSlideRecord
+                        var questionnaire = new QuestionnaireModel()
+                        {
+                            allowMultiple = window.AllowMultipleCheck.IsChecked == true,
+                            choices = (int)window.ChoicesComboBox.SelectionBoxItem
+                        };
+
+                        CreateQuestionnaireSlideRecord(slide, questionnaire);
+                    }
+                    else
+                    {
+                        //UpdateSelectedSlide
+
+                        var questionnaire = _questionnaireUtil.Deserialize(slide);
+
+                        questionnaire.allowMultiple = window.AllowMultipleCheck.IsChecked == true;
+                        questionnaire.choices = (int)window.ChoicesComboBox.SelectionBoxItem;
+
+                        UpdateSelectedSlide(slide, questionnaire);
+                    }
+                }
+                catch
+                {
+                    _questionnaireUtil.Unmark(slide);
+                    MessageBox.Show("问卷设置失败了,这可能是个网络错误");
+                }
+                finally
+                {
+                    RefreshSetAndAdd();
+                }
+
+            }
+        }
+
+        private async void buttonCancel_Click(object sender, RibbonControlEventArgs e)
+        {
+            var questionnaire = _questionnaireUtil.Deserialize(_selectedSlide);
+            try
+            {
+                await _questionnaireUtil.DeleteAsync(questionnaire.id);
+            }
+            catch
+            {
+            }
+            finally
+            {
+                _questionnaireUtil.Unmark(_selectedSlide);
+
+                RefreshSetAndAdd();
+            }
+        }
+    }
+}
